b5d3f3f44dd4a94d205c6be282117fe4a92ac069
Łukasz Gonczarik <lukasz139@gmail.com>
2016-04-12 14:26:27 +0200

    simple lectuers

--- PlanZajec/PlanZajec/ViewModels/ProwadzacyViewModel.cs
+++ PlanZajec/PlanZajec/ViewModels/ProwadzacyViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,30 @@ using PlanZajec.DataModel;
 
 namespace PlanZajec.ViewModels
 {
-    public class ProwadzacyViewModel : ViewModel
+    public class ProwadzacyViewModel : INotifyPropertyChanged
     {
         public List<Prowadzacy> Items { get; set; }
+        public List<string> NazwyProwadzacych { get; set; } 
 
         public ProwadzacyViewModel()
         {
             using (var uw = new UnitOfWork(new PlanPwrContext()))
             {
                 Items = uw.Prowadzacy.GetAll().ToList();
+                NazwyProwadzacych = new List<string>();
+                foreach (var prow in Items)
+                {
+                    NazwyProwadzacych.Add(prow.Imie + " " + prow.Nazwisko + " " + prow.Tytul);
+                }
             }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
+
