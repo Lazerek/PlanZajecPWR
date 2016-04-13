984a22963d35966f62fe0aedeac91bce9641b7ff
Lazerek <lazarski.mariusz@gmail.com>
2016-04-13 17:54:55 +0200

    Naprawa bledow

--- PlanZajec/PlanZajec/Views/OknoOpcji.xaml.cs
+++ PlanZajec/PlanZajec/Views/OknoOpcji.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using PlanZajec.Views;
-using PlanZajec.ViewModels;
 using Wpf;
 
 namespace PlanZajec.Views
