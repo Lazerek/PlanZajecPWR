5845fa8bdc696ca7c4a0d1c764272488b4dc809e
Lazerek <lazarski.mariusz@gmail.com>
2016-04-13 18:13:20 +0200

    Naprawa filtrowania

---
+++ PlanZajec/PlanZajec/ViewModels/ProwadzcyOpinieViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlanZajec.DataAccessLayer;
+using PlanZajec.DataModel;
+
+namespace PlanZajec.ViewModels
+{
+    public class ProwadzacyOpinieViewModel : INotifyPropertyChanged
+    {
+        public List<Prowadzacy> Items { get; set; }
+        public List<Prowadzacy> ComboBoxItems { get; set; }
+
+        public ProwadzacyOpinieViewModel()
+        {
+            ComboBoxItems = new List<Prowadzacy>();
+            using (var uw = new UnitOfWork(new PlanPwrContext()))
+            {
+                ComboBoxItems = uw.Prowadzacy.GetAll().ToList();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
---
+++ PlanZajec/PlanZajec/Views/ProwadzacyOpinie.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PlanZajec.ViewModels;
+using PlanZajec.DataModel;
+
+namespace PlanZajec
+{
+    /// <summary>
+    /// Interaction logic for ProwadzacyMenu.xaml
+    /// </summary>
+    public partial class ProwadzacyOpinie : UserControl
+    {
+        public ProwadzacyOpinieViewModel ViewModel;
+        public ProwadzacyOpinie()
+        {
+            InitializeComponent();
+            ViewModel = new ProwadzacyOpinieViewModel();
+            DataContext = ViewModel;
+            foreach(Prowadzacy pr in ViewModel.ComboBoxItems)
+                comboBox.Items.Add(pr.Tytul + " " + pr.Imie + " " + pr.Nazwisko);
+            for (int i = 1; i < 11; i++)
+                comboBox1.Items.Add(i);
+            int rowIndex = comboBox.SelectedIndex;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
