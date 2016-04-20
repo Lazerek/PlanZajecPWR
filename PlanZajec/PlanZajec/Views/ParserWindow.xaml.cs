87330adb6b54719e3034e78753b5711fcc154969
Łukasz Gonczarik <lukasz139@gmail.com>
2016-04-20 19:02:05 +0200

    clear

--- PlanZajec/PlanZajec/Views/ParserWindow.xaml.cs
+++ PlanZajec/PlanZajec/Views/ParserWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PlanZajec.Views
 {
@@ -27,6 +15,7 @@ namespace PlanZajec.Views
         private void RunParserButton_OnClick(object sender, RoutedEventArgs e)
         {
             Parser.Parser.Run();
+            
         }
     }
 }
