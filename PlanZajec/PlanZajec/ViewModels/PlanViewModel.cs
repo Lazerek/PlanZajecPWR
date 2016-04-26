7e7892ffa78907013c491a8e6736489ff90ede85
JerzySzachniewicz <Jerzy.Szachniewicz@02.pl>
2016-04-26 15:36:56 +0200

    ViewModel plan

---
+++ PlanZajec/PlanZajec/ViewModels/PlanViewModel.cs
@@ -0,0 +1,25 @@
+﻿using PlanZajec.DataAccessLayer;
+using PlanZajec.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanZajec.ViewModels
+{
+    class PlanViewModel
+    {
+        public List<GrupyZajeciowe> ListaGrupZajeciowych { get; private set; }
+
+        public PlanViewModel()
+        {
+            using (var uw = new UnitOfWork(new PlanPwrContext()))
+            {
+                ListaGrupZajeciowych = uw.Plany.Get(1).GrupyZajeciowe.ToList();
+               
+                //ListaGrupZajeciowych[0];
+            }
+        }
+    }
+}
