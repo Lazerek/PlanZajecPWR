9123b541ada24407e599876b437375c239063ef1
JerzySzachniewicz <Jerzy.Szachniewicz@02.pl>
2016-04-26 19:32:44 +0200

    Super push armagednu: Graficzny wygląd planu

--- PlanZajec/PlanZajec/ViewModels/PlanViewModel.cs
+++ PlanZajec/PlanZajec/ViewModels/PlanViewModel.cs
@@ -1,23 +1,35 @@
 ﻿using PlanZajec.DataAccessLayer;
 using PlanZajec.DataModel;
+using PlanZajec.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Wpf;
 
 namespace PlanZajec.ViewModels
 {
     class PlanViewModel
     {
         public List<GrupyZajeciowe> ListaGrupZajeciowych { get; private set; }
-
+        public List<GrupaZajeciowaTemplate> Kafelki { get; private set; }
         public PlanViewModel()
         {
             using (var uw = new UnitOfWork(new PlanPwrContext()))
             {
+                Kafelki=new List<GrupaZajeciowaTemplate>();
                 ListaGrupZajeciowych = uw.Plany.Get(1).GrupyZajeciowe.ToList();
-               
+                foreach(GrupyZajeciowe gr in ListaGrupZajeciowych)
+                {
+                    var temp = new GrupaZajeciowaTemplate(gr.KodGrupy);
+                    
+                   var okno= Application.Current.Windows.OfType<MainWindow>().First();
+                    var a = okno.DataContext as PlanView;
+                    System.Diagnostics.Debug.WriteLine(a);
+                    Kafelki.Add(temp);
+                }
                 //ListaGrupZajeciowych[0];
             }
         }
