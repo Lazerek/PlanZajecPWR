83613f4445ef765bb1a49c731916af53ee85f173
Łukasz Gonczarik <lukasz139@gmail.com>
2016-05-02 13:34:29 +0200

    Plan as grid and cell merge

--- PlanZajec/PlanZajec/Views/PlanView.xaml.cs
+++ PlanZajec/PlanZajec/Views/PlanView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using PlanZajec.ViewModels;
 
@@ -14,15 +15,18 @@ namespace PlanZajec.Views
         public PlanView()
         {
             InitializeComponent();
-            TabelaGrup.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            //Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+            //var t = DesiredSize.Width;
+
             viewModel = new PlanViewModel();
             DataContext = viewModel;
-            rysujPlan();
+            NarysujPlan();
             pv = this;
            
            
         }
-        private void rysujPlan()
+
+        private void RysujPlan()
         {
             for (var i = 0; i < viewModel.Kafelki.Count; i++)
             {
@@ -55,7 +59,6 @@ namespace PlanZajec.Views
                 var godzina = viewModel.ListaGrupZajeciowych[i].Godzina;
                 var tempS = godzina.Split(':');
 
-
                 double t = 3200;
                 tempC = (t / (60 * 14) * (int.Parse(tempS[0]) * 60 + int.Parse(tempS[1]))) - 7 * 60 - (t - 400);
 
@@ -65,12 +68,56 @@ namespace PlanZajec.Views
                 var margin = tempChild.Margin;
                 margin.Left = (int)tempC;
                 tempChild.Margin = margin;
-             
-                         
-                
             }
-
         }
+
+        private void NarysujPlan()
+        {
+            for (var i = 0; i < viewModel.Kafelki.Count; i++)
+            {
+                var kafelek = viewModel.Kafelki[i];
+                int numerDnia = 0;
+                switch (viewModel.ListaGrupZajeciowych[i].Dzień)
+                {
+                    case "pn":
+                        numerDnia = 0;
+                        break;
+                    case "wt":
+                        numerDnia = 1;
+                        break;
+                    case "śr":
+                        numerDnia = 2;
+                        break;
+                    case "cz":
+                        numerDnia = 3;
+                        break;
+                    case "pt":
+                        numerDnia = 4;
+                        break;
+                    case "sb":
+                        numerDnia = 5;
+                        break;
+                    case "nd":
+                        numerDnia = 6;
+                        break;
+                }
+                string godzinaRozpoczeciaString = viewModel.ListaGrupZajeciowych[i].Godzina;
+                int godzinaRozpoczecia = int.Parse(godzinaRozpoczeciaString.Split(':')[0])-1;
+                string godzinaZakonczeniaString = viewModel.ListaGrupZajeciowych[i].GodzinaKoniec;
+                int godzinaZakonczenia = int.Parse(godzinaZakonczeniaString.Split(':')[0])-1;
+                int czasTrwania = godzinaZakonczenia - godzinaRozpoczecia;
+                
+                //var t = TabelaGrup.ColumnDefinitions.First(e => Grid.GetColumn(e) == godzinaRozpoczecia);
+                //var uiElement = TabelaGrup.Children;//.Cast<UIElement>().ToArray();//.First(e => Grid.GetRow(e) == numerDnia && Grid.GetColumn(e) == godzinaRozpoczecia);
+                
+
+                TabelaGrup.Children.Add(kafelek);
+                Grid.SetRow(kafelek, numerDnia);
+                Grid.SetColumn(kafelek, godzinaRozpoczecia-6);
+                Grid.SetColumnSpan(kafelek, czasTrwania);
+            }
+        }
+
         private void usun()
         {
             foreach(var gr in viewModel.Kafelki)
@@ -84,7 +131,7 @@ namespace PlanZajec.Views
             pv.viewModel = new PlanViewModel();
             pv.DataContext = pv.viewModel;
             pv.usun();
-            pv.rysujPlan();
+            pv.NarysujPlan();
         }
     }
 }
