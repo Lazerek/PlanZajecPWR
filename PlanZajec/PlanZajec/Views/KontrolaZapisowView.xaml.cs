77b2a9987125829d73253b4ebb5f8ae1a9df3ba1
Łukasz Gonczarik <lukasz139@gmail.com>
2016-05-10 19:07:16 +0200

    Courses in schedule check

---
+++ PlanZajec/PlanZajec/Views/KontrolaZapisowView.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PlanZajec.DataAccessLayer;
+using PlanZajec.DataModel;
+
+namespace PlanZajec.Views
+{
+    /// <summary>
+    /// Interaction logic for KontrolaZapisowView.xaml
+    /// </summary>
+    public partial class KontrolaZapisowView : UserControl
+    {
+        public KontrolaZapisowView()
+        {
+            InitializeComponent();
+            List<Expander> schedulesExpanders = new List<Expander>();
+            using (var uw = new UnitOfWork(new PlanPwrContext()))
+            {
+                var schedules = uw.Plany.GetAll().ToList();
+                foreach (var schedule in schedules)
+                {
+                    Expander exp = new Expander();
+                    exp.Header = "Schedule " + schedule.IdPlanu + " control";
+                    StackPanel tpanel = new StackPanel();
+
+                    var allCourses = uw.Kursy.GetAll().ToList();
+                    var groupsInScheduleCodes = schedule.GrupyZajeciowe.Select(grupa => grupa.KodKursu).ToList();
+
+                    foreach (var course in allCourses)
+                    {
+                        Color inScheduleColor;
+                        if (groupsInScheduleCodes.Contains(course.KodKursu))
+                        {
+                            inScheduleColor = Colors.Green;
+                        }
+                        else
+                        {
+                            inScheduleColor = Colors.DarkRed;
+                        }
+                        TextBlock courseTextBlock = new TextBlock {Background = new SolidColorBrush(inScheduleColor), Text = course.NazwaKursu};
+
+                        tpanel.Children.Add(courseTextBlock);
+                    }
+
+                    exp.Content = tpanel;
+                    KontroleZapisowPanel.Children.Add(exp);
+                }
+            }
+        }
+    }
+}
